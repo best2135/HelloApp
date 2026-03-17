ebdda03e3e5e882799e32d34e152671211d03618
Dhruv k <dk8159@srmist.edu.in>
2026-03-17 16:33:40 +0530

    uc4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,12 +1,21 @@
-public class HelloApp{
-    public static void main(String[] args){
+public class HelloApp {
+    public static void main(String[] args) {
         String defname = "World";
-        if(args.length > 0){
-            String name = args[0];
-            System.out.println("Hello, "+ name + "!");
-            return;
-        }
-        System.out.println("Hello, "+ defname + "!");
 
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                sb.append(args[i]);
+
+                if (i < args.length - 1) {   // we only need to add comma between items
+                    sb.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + sb + "!");
+        } else {
+            System.out.println("Hello, " + defname + "!");
+        }
     }
 }
